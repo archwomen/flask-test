5bd5d79efb9ee0d8d05317f9326a9c607336bff4
meskarune <admin@doloresportalatin.info>
2016-04-10 17:07:01 -0400

    removed non-working code

--- app/views.py
+++ app/views.py
@@ -25,26 +25,8 @@ def markdown_filter(text):
     """ Convert markdown to html """
     return Markup(markdown(text, extensions=[CodeHiliteExtension(linenums=True, css_class='highlight'), ExtraExtension()]))
 
-def get_post_dir():
-    return os.path.dirname(os.path.abspath(__file__)) + '/content/posts'
-
-def get_post_items():
-    items = os.listdir(get_post_dir())
-    items.sort(reverse=True)
-    return items
-
-def get_posts():
-    posts = []
-    for item in get_post_items():
-        posts.append(item)
-    return posts
-
 @app.route('/')
-#@app.route('/', methods=['GET', 'POST'])
-#@app.route('/index', methods=['GET', 'POST'])
-#@app.route('/index/<int:page>', methods=['GET', 'POST'])
 def index():
-    #post_dir = 'app/content/posts/%s%s'%(path, '.md')
     return render_template('index.html', title='Home')
 
 @app.route('/<path:path>/')
@@ -56,24 +38,6 @@ def page(path):
             return render_template('page.html', page_html=content)
     else:
         abort(404)
-
-@app.route('/blog/')
-def blog():
-    content = get_posts()
-    return render_template('blog.html', page_html=content)
-
-#@app.route('/blog/<int:year>/<int:month>/<slug>')
-
-#@app.route('/blog/<tag>/<slug>/')
-
-#@app.route('/<int:year>/')
-#def year_index(year):
-#    year_dir = 'app/content/posts/%s'%(year)
-#    if os.path.isdir(year_dir):
-#        entries = filter(path.isfile, os.listdir(year_dir))
-#        return render_template('yearindex.html', page_html=entries)
-#    else:
-#        abort(404)
 
 @app.route('/feed')
 @app.route('/feed.atom')
