490b6ec828acc122cf75d841b7bb3a40c74df54e
Dolores Portalatin <admin@doloresportalatin.info>
2016-07-17 22:24:17 -0400

    added code to get flask working

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.cfg')
 from app import views
+app.wsgi_app = ProxyFix(app.wsgi_app)
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 #!flask-website/bin/python
+
 from app import app
+
+
 app.secret_key = 'secret'
-app.run(debug=True, host='0.0.0.0')
+app.run()
+#app.run(debug=True, host='0.0.0.0')
